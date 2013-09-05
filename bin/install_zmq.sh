ce9d0fd13b30cf58ee1469c4738b8f810dc0a911
Dieterich Lawson <dieterich.lawson@gmail.com>
2013-09-05 10:43:41 -0500

    Updated install_zmq comments for accuracy

--- bin/install_zmq.sh
+++ bin/install_zmq.sh
@@ -3,11 +3,13 @@ export JAVA_HOME=${JAVA_HOME:/usr/libexec/java_home}
 
 if [ ! -d "$JAVA_HOME/include" ]; then
     echo "
-Looks like you're missing your 'include' directory. If you're using Mac OS X, You'll need to install the Java dev package.
+    Looks like you're missing your 'include' directory. If you're using Mac OS X, You'll need to install the Java dev package (JDK).
 
-- Navigate to http://goo.gl/D8lI
-- Click the Java tab on the right
-- Install the appropriate version and try again.
+- Navigate to http://goo.gl/UEOm8b (JDK 7u25 download link, use a more recent one if applicable)
+- Install the appropriate version
+- Make sure the JDK is on your path
+- Try again
+
 "
     exit -1;
 fi
